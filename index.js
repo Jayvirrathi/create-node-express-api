deae9422b22ecccc034c72e478bee1c5fd5286f8
Jayvir Rathi <jayvirrathi@gmail.com>
2020-11-06 11:17:46 +0530

    added inquirer & repo url ✨🤞

--- index.js
+++ index.js
@@ -1,62 +1,96 @@
 #! /usr/bin/env node
 
 const { spawn } = require("child_process");
+const inquirer = require("inquirer");
+const path = require("path");
+const configFiles = ["nodejs", "nodejs-babel", "nodejs-typescript"];
+const packageManager = ["npm", "yarn"];
 
-const name = process.argv[2];
-const yarnFlag = process.argv[3];
-if (!name || name.match(/[<>:"\/\\|?*\x00-\x1F]/)) {
-  return console.log(`
-  Invalid directory name.
-  Usage: create-node-express-api name-of-api  
-`);
-}
+const nodeExpressRepoURL =
+  "https://github.com/Jayvirrathi/node-express-api-starter.git";
+const nodeExpressBabelRepoURL =
+  "https://github.com/Jayvirrathi/node-express-es6-api-starter.git";
+const nodeExpressTypeScriptRepoURL =
+  "https://github.com/Jayvirrathi/node-express-typescript-api-starter.git";
 
-if (yarnFlag) {
-  if (!yarnFlag || !yarnFlag.match(/^-(-yarn|y)$/)) {
-    return console.log(`
-    Invalid option flag.
-    Usage: create-node-express-api name-of-api [-y|--yarn]
-  `);
+let repoURL;
+
+(async () => {
+  const { framework } = await inquirer.prompt([
+    {
+      type: "list",
+      message: "Pick the framework you're using:",
+      name: "framework",
+      choices: configFiles,
+    },
+  ]);
+
+  console.log(framework);
+
+  if (framework === "nodejs") {
+    repoURL = nodeExpressRepoURL;
+  } else if (framework === "nodejs-babel") {
+    repoURL = nodeExpressBabelRepoURL;
+  } else if (framework === "nodejs-typescript") {
+    repoURL = nodeExpressTypeScriptRepoURL;
+  } else {
+    repoURL = nodeExpressRepoURL;
   }
-}
-const repoURL = "https://github.com/Jayvirrathi/node-express-api-starter.git";
 
-runCommand("git", ["clone", repoURL, name])
-  .then(() => {
-    return runCommand("rm", ["-rf", `${name}/.git`]);
-  })
-  .then(() => {
-    console.log("Installing dependencies...");
-    return runCommand(
-      yarnFlag ? "yarn.cmd" : "npm.cmd",
-      [yarnFlag ? "" : "install"],
-      {
+  const { name } = await inquirer.prompt([
+    {
+      type: "input",
+      message: "Pick the name:",
+      name: "name",
+      validate: function (name) {
+        let valid = !(name == "");
+        return valid || `Please enter a valid name`;
+      },
+    },
+  ]);
+
+  const { tool } = await inquirer.prompt([
+    {
+      type: "list",
+      choices: packageManager,
+      message: "Pick the package manager:",
+      name: "tool",
+    },
+  ]);
+
+  runCommand("git", ["clone", repoURL, name])
+    .then(() => {
+      return runCommand("rm", ["-rf", `${name}/.git`]);
+    })
+    .then(() => {
+      console.log("Installing dependencies...");
+      return runCommand(tool == "yarn" ? "yarn.cmd" : "npm.cmd", ["install"], {
         cwd: process.cwd() + "/" + name,
-      }
-    );
-  })
-  .then(() => {
-    console.log("Done! 🏁");
-    console.log("");
-    console.log("To get started:");
-    console.log("cd", name);
-    console.log(yarnFlag ? "yarn dev" : "npm run dev");
-  });
-
-function runCommand(command, args, options = undefined) {
-  const spawned = spawn(command, args, options);
-
-  return new Promise((resolve) => {
-    spawned.stdout.on("data", (data) => {
-      console.log(data.toString());
+      });
+    })
+    .then(() => {
+      console.log("Done! 🏁");
+      console.log("");
+      console.log("To get started:");
+      console.log("cd", name);
+      console.log(tool == "yarn" ? "yarn dev" : "npm run dev");
     });
 
-    spawned.stderr.on("data", (data) => {
-      console.error(data.toString());
-    });
+  function runCommand(command, args, options = undefined) {
+    const spawned = spawn(command, args, options);
 
-    spawned.on("close", () => {
-      resolve();
+    return new Promise((resolve) => {
+      spawned.stdout.on("data", (data) => {
+        console.log(data.toString());
+      });
+
+      spawned.stderr.on("data", (data) => {
+        console.error(data.toString());
+      });
+
+      spawned.on("close", () => {
+        resolve();
+      });
     });
-  });
-}
+  }
+})();
